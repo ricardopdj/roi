de0533149f6efa2a45589a87a8fbbfa91f8b5d51
Ricardo <pareja.r@gmail.com>
2019-05-19 18:35:02 -0300

    Init todo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
 import * as TodoAPI from './TodoAPI'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+library.add(faTrash)
 
-class App extends Component {
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      todos: null,
+      error: null
+    }
+  }
+
   componentDidMount () {
     this.initSession()
   }
@@ -12,30 +23,94 @@ class App extends Component {
     TodoAPI
       .initSession()
       .then((data) => {
-        console.log(data)
+        this.getTodos(data.sessionId)
       })
       .catch((error) => {
         console.log(error)
       })
   }
 
+  getTodos (sessionId) {
+    TodoAPI
+      .getAll(sessionId)
+      .then((data) => {
+        switch (data.status) {
+          case 'OK':
+            this.setState({ todos: data.todos })
+            console.log(this.state)
+            break
+          case 'ERROR':
+            this.setState({ error: data.error })
+            break
+          default:
+            console.log(data)
+            break
+        }
+        console.log(this.state.todos)
+      })
+      .catch((error) => {
+        console.log('error', error)
+      })
+  }
+
   render () {
+    const todos = this.state.todos
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
+        <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+          <div className='container'>
+            <a className='navbar-brand' href='#'>TodoROI</a>
+            <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+              <span className='navbar-toggler-icon' />
+            </button>
+
+            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+              <ul className='navbar-nav mr-auto'>
+                <li className='nav-item active'>
+                  <a className='nav-link' href='#'>Home <span className='sr-only'>(current)</span></a>
+                </li>
+              </ul>
+              <form className='form-inline my-2 my-lg-0'>
+                <input className='form-control mr-sm-2' type='search' placeholder='Search' aria-label='Search' />
+                <button className='btn btn-outline-light my-2 my-sm-0' type='submit'>Search</button>
+              </form>
+            </div>
+          </div>
+        </nav>
+        <div className='container py-5'>
+          {
+            this.state.error &&
+            <p>{this.state.error}</p>
+          }
+          {
+            todos &&
+            Object.values(todos).map((todo, index) =>
+              <div className='input-group mb-3 task' key={index}>
+                <div className='input-group-prepend'>
+                  <div className='input-group-text'>
+                    <input
+                      type='checkbox'
+                      aria-label='Checkbox for following text input'
+                      defaultChecked={todo.isCompleted}
+                    />
+                  </div>
+                </div>
+                <input
+                  type='text'
+                  className='form-control task-name'
+                  aria-label='Text input with checkbox'
+                  value={todo.text}
+                  readOnly
+                />
+                <div className='input-group-append'>
+                  <button className='btn btn-outline-secondary' type='button'>
+                    <FontAwesomeIcon icon='trash' />
+                  </button>
+                </div>
+              </div>
+            )
+          }
+        </div>
       </div>
     )
   }
--- src/TodoAPI.js
+++ src/TodoAPI.js
@@ -1,10 +1,22 @@
 const api = 'http://localhost:9000/api'
 
-export const initSession = () => fetch(`${api}/session`, { method: 'post' })
-  .then(res => res.json())
-  .then((data) => {
-    return data
-  })
-  .catch(err => {
-    throw err
-  })
+export const initSession = () =>
+  fetch(`${api}/session`, { method: 'post' })
+    .then(res => res.json())
+    .catch(err => {
+      throw err
+    })
+
+export const getAll = (sessionId) =>
+  fetch(`${api}/todos`,
+    {
+      headers: {
+        sessionId: sessionId
+      },
+      method: 'GET',
+      mode: 'cors'
+    })
+    .then(res => res.json())
+    .catch(err => {
+      throw err
+    })
